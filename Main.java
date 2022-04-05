d9c3f3f7767e084676b638175e6d94000d5b5029
Adams505 <101186913+Adams505@users.noreply.github.com>
2022-04-05 11:26:25 +0000

    poprawa println

--- Main.java
+++ Main.java
@@ -5,11 +5,12 @@ class Main {
     int h=4;
     for(int i=1;i<=h;i++){
       for(int j=h;j>=i;j--)System.out.print(" ");
-      for(int j=i;j>0;j--)System.out.println("*");
+      for(int j=i;j>0;j--)System.out.print("*");
+      System.out.println(" ");
      }
     for(int i=1; i<=h;i++)
       {
-System.out.print(" ");
+      System.out.print(" ");
         for(int j=1;j<=i;j++)System.out.print("*");
         System.out.println(" ");
       }
